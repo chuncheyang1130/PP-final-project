bbf5aceaa51a89576961d1a313f50f7c65df1157
angusyang901130 <ccy.angus1130@gmail.com>
2023-12-21 23:09:50 +0800

    remove '\r'

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #### bellmanford seriol ####
 ./bellmanford_serial ./output.txt $1 $2
-
 ### bellmanford thread ####
 ./bellmanford_thread ./output.txt $1 $2
-
 ### solution ###
 python3 ./tools/solution.py -s $1 -d $2
