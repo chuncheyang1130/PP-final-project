04a7e8a98c4410c48567296824090dd4a8bc1e8c
chuncheyang1130 <ccy.angus1130@gmail.com>
2023-12-22 16:54:49 +0800

    bellmanford cuda init

--- bellmanford.cu
+++ bellmanford.cu
@@ -14,63 +14,49 @@ struct Edge{
     int weight;
 };
 
-__global__ BellmanFordKernel(std::vector<Edge>& edges, int srcNode, int dstNode, int numNodes){
+__global__ BellmanFordKernel(Edge* edges, int* dist, int numNodes, int numEdges){
 
-    std::vector<int> dist(numNodes, INF);
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
 
-    int N = numNodes - 1;
-    dist[srcNode] = 0;
+    if (threadId >= numEdges)
+        return;
 
-    int edge_num = edges.size();
+    edge = edges[threadId];
 
-    // while (N--){
-    //     for (const auto& edge : edges){
-    //         if (dist[edge.src] != INF)
-    //             dist[edge.dst] = std::min(dist[edge.src] + edge.weight, dist[edge.dst]);
-    //     }
-    // }
-
-    omp_set_num_threads(4);
-
-    std::vector<std::vector<int>> local_dist(4, std::vector<int>(dist));
-
-    while (N--) {
-
-        // for(int i = 0; i < numNodes; i++){
-        //     std::printf("local_dist[%d] = %d\n", i, local_dist[0][i]);
-        // }
-        // std::printf("\n");
-
-        #pragma omp parallel
-        {
-            int id = omp_get_thread_num();            
-
-            #pragma omp for
-            for (int i = 0; i < edge_num; i++){
-                Edge edge = edges[i];
-
-                if (local_dist[id][edge.src] != INF){
-                    local_dist[id][edge.dst] = std::min(local_dist[id][edge.src] + edge.weight, local_dist[id][edge.dst]);
-                }      
-            }
-            
-            #pragma omp for
-            for (int i = 0; i < numNodes; i++){
-                dist[i] = std::min({local_dist[0][i], local_dist[1][i], local_dist[2][i], local_dist[3][i]});
-                // std::printf("dist[%d] = %d\n", i, dist[i]);
-            }
-
-            for (int i = 0; i < numNodes; i++){
-                local_dist[id][i] = dist[i];
-            }
-            
-        }
-
+    if (dist[edge.src] != INF/*  && dist[edge.src] + edge.weight < dist[edge.dst] */)
+        atomicMin(&dist[edge.dst], dist[edge.src] + edge.weight);
         
+    
+}
+
+int BellmanFord(Edge* edges, int srcNode, int dstNode, int numNodes, int numEdges){
+    Edge* deviceEdgeArray;
+    cudaMalloc(&deviceEdgeArray, numEdges * sizeof(struct Edge));
+    cudaMemcpy(devcieEdgeArray, edges, numEdges * sizeof(struct Edge), cudaMemcpyHostToDevice);
+    
+    int* dist = (int*)malloc(numNodes * sizeof(int));
+    
+    for (int i = 0; i < numNodes; i++){
+        dist[i] = INF;
     }
 
-    // #pragma omp parallel for
-    for (int i = 0; i < edge_num; i++){
+    dist[srcNode] = 0;
+
+    int* deviceDistArray;
+    cudaMalloc(&deviceDistArray, numNodes * sizeof(int));
+    cudaMemcpy(deviceDistArray, dist, numNodes * sizeof(int), cudaMemcpyHostToDevice);
+    
+    int N = numNodes - 1;
+    int threadPerBlock = 32;
+    int numBlocks = (numEdges + threadPerBlock - 1) / threadPerBlock;
+
+    while(N--){
+        BellmanFordKernel<<<numBlocks, threadPerBlock>>>(deviceEdgeArray, deviceDistArray, numNodes, numEdges);
+    }
+
+    cudaMemcpy(dist, deviceDistArray, numNodes * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < numEdges; i++){
         Edge edge = edges[i];
         if (dist[edge.src] != INF && dist[edge.dst] > dist[edge.src] + edge.weight){
             std::printf("NEGATIVE CYCLE !!\n");
@@ -78,14 +64,13 @@ __global__ BellmanFordKernel(std::vector<Edge>& edges, int srcNode, int dstNode,
         }
     }
 
-    return dist[dstNode];
+    cudaFree(deviceEdgeArray);
+    cudaFree(deviceDistArray);
     
-}
+    int min_dist = dist[dstNode];
+    free(dist);
 
-int BellmanFord(Edge* edges, int srcNode, int dstNode, int numNodes, int numEdges){
-    Edge* deviceArray;
-    cudaMalloc(&deviceArray, numEdges * sizeof(struct Edge));
-
+    return min_dist;
     
 }
 
@@ -124,10 +109,10 @@ int main(int argc, char *argv[]) {
         // Edge e1 = {source, target, weight};
         // edges.push_back(e1);
         edges[i*2].src = source;
-        edges[i*2+1].src = source;
+        edges[i*2+1].src = target;
 
         edges[i*2].dst = target;
-        edges[i*2+1].dst = target;
+        edges[i*2+1].dst = source;
 
         edges[i*2].weight = weight;
         edges[i*2+1].weight = weight;
