1ca1c03041caafb3ae2d11ce6d7b51c3e9b69469
angusyang901130 <ccy.angus1130@gmail.com>
2023-12-29 00:27:16 +0800

    new run.sh

---
+++ run_bellmanford.sh
@@ -0,0 +1,51 @@
+echo -e "=== Scale 1 ==="
+#### bellmanford seriol ####
+./bellmanford_serial ./graph8192_16384 $1 $2
+
+#### bellmanford thread ####
+./bellmanford_thread ./graph8192_16384 $1 $2
+
+#### bellmanford cuda ####
+# ./bellmanford_cuda ./graph10000_20000 $1 $2
+
+#### solution ####
+python3 ./tools/solution.py -s $1 -d $2 -f ./graph8192_16384.pkl
+
+echo -e "=== Scale 2 ==="
+#### bellmanford seriol ####
+./bellmanford_serial ./graph8192_32768 $1 $2
+
+#### bellmanford thread ####
+./bellmanford_thread ./graph8192_32768 $1 $2
+
+#### bellmanford cuda ####
+# ./bellmanford_cuda ./graph10000_40000 $1 $2
+
+#### solution ####
+python3 ./tools/solution.py -s $1 -d $2 -f ./graph8192_32768.pkl
+
+echo -e "=== Scale 3 ==="
+#### bellmanford seriol ####
+./bellmanford_serial ./graph8192_65536 $1 $2
+
+#### bellmanford thread ####
+./bellmanford_thread ./graph8192_65536 $1 $2
+
+#### bellmanford cuda ####
+# ./bellmanford_cuda ./graph10000_40000 $1 $2
+
+#### solution ####
+python3 ./tools/solution.py -s $1 -d $2 -f ./graph8192_65536.pkl
+
+echo -e "=== Scale 4 ==="
+#### bellmanford seriol ####
+./bellmanford_serial ./graph8192_131072 $1 $2
+
+#### bellmanford thread ####
+./bellmanford_thread ./graph8192_131072 $1 $2
+
+#### bellmanford cuda ####
+# ./bellmanford_cuda ./graph10000_40000 $1 $2
+
+#### solution ####
+python3 ./tools/solution.py -s $1 -d $2 -f ./graph8192_131072.pkl
